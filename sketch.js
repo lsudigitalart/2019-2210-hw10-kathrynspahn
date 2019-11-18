bad7acdddbfd59ec08e57caf02890695ca4e75e3
kathrynspahn <54597353+kathrynspahn@users.noreply.github.com>
2019-11-18 17:36:32 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,97 @@
-tyler = [];
+var tyler1;
+var tyler2;
+var tyler3;
+var tyler4;
+var face1, face2, face3, face4;
+//var pic;
 
-function preload(){
-  tyler = loadImage("images/tyler-3.jpg");
+var randYPos1;
+var randYPos2;
+var randYPos3;
+var randYPos4;
+
+function preload() {
+  face1 = loadImage("images/tyler.jpg");
+  face2 = loadImage("images/tyler-2.jpg");
+  face3 = loadImage("images/tyler-3.jpg");
+  face4 = loadImage("images/tyler-4.jpg");
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  randYPos1 = random(height);
+  randYPos2 = random(height);
+  randYPos3 = random(height);
+  randYPos4 = random(height);
+
+
+
+  tyler1 = new Meme(face1,1, randYPos1, 0, 2);
+  tyler2 = new Meme(face2,1,randYPos2, 0, 5);
+  tyler3 = new Meme(face3,1, randYPos3, 0, 10);
+  tyler4 = new Meme(face4,1, randYPos4, 0, 3);
+
 }
 
 function draw() {
   background(0);
-  for (let i = 0; i < 5; i++) {
-    let t = new tyler();
-    tyler.push(t);
-  }
-  for (let i = tyler.length - 1; i >= 0; i--) {
-    tyler[i].update();
-    tyler[i].show();
-    if (tyler[i].finished()) {
-      // remove this particle
-      tyler.splice(i, 1);
-    }
-  }
+
+  tyler1.update();
+  tyler1.display();
+
+  tyler2.update();
+  tyler2.display();
+
+  tyler3.update();
+  tyler3.display();
+
+  tyler4.update();
+  tyler4.display();
+
+  let t = 'WHICH TYLER IS THE FASTEST?';
+fill(255);
+text(t, 150, 150); 
+
 }
 
-class tyler {
+function Meme(pic,tempSize, tempY, tempX,tempSpeed) {
+  this.pic = pic;
+  this.size = tempSize;
+  //this.colorR = tempColorR;
+  this.yPos = tempY;
+  this.xPos = tempX;
+  //this.accel = accel;
+  this.speed = tempSpeed;
 
-  constructor() {
-    this.x = 300;
-    this.y = 380;
-    this.vx = random(-1, 1);
-    this.vy = random(-5, -1);
-    this.alpha = 255;
+  this.update = function () {
+    this.xPos = this.xPos + this.speed;
+
+    if (this.xPos > width) {
+      this.xPos +=1; 
+    }
+
   }
 
-  finished() {
-    return this.alpha < 0;
-  }
+  this.display = function () {
+    //var pic = [face1,face2,face3,face4];
 
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    this.alpha -= 5;
+    image(pic,this.size * 0.5 + this.xPos,randYPos1);
+    //image(face2,this.size * 0.5 + this.xPos,randYPos2);
+    //image(face3,this.size * 0.5 + this.xPos,randYPos3);
+    //image(face4,this.size * 0.5 + this.xPos,randYPos4);
+    
   }
+ /* this.interact = function()
+  {
+      if (keyIsPressed === true)
+      {
+        accel += 1;
+        this.accel = accel;
+      }
+  }*/
 
-  show() {
-    noStroke();
-    //stroke(255);
-    fill(255, this.alpha);
-    ellipse(this.x, this.y, 16);
-  }
+
+  
 
 }
